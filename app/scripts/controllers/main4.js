81702354f05ba29c066270df3ce900fc6ce2c84f
brookebc <brooke.casey@gmail.com>
2014-06-16 20:10:06 -0400

    user is now geolocated and can add a marker for the user as well as a marker for the beaches that are listed on that page

--- app/scripts/controllers/main4.js
+++ app/scripts/controllers/main4.js
@@ -9,40 +9,73 @@ angular.module('gitNgbeachyApp')
     ];
   })
 
-  .controller('geoCtrl', [ '$scope', 'geolocation', function ($scope,geolocation) {
+  .controller('geoCtrl', [ '$scope', 'geolocation', 'leafletEvents', function ($scope, geolocation, leafletEvents) {
     
-    $scope.center = {
-    	lat: 38.8833,
-	    lng: -77.0167,
-        zoom: 4
-    }
+    angular.extend($scope, {
+   		center : {
+    		lat: 38.8833,
+	    	lng: -77.0167,
+        	zoom: 4
+    	},
+    	defaults: {
+    		scrollWheelZoom: false
+    	},
+    	markers: {}
+    });
 
     geolocation.getLocation().then(function(data){
 
-    		$scope.center = {
+    	$scope.center = {
     			lat: data.coords.latitude, 
     			lng: data.coords.longitude, 
-    			zoom: 12
+    			zoom: 13
     			}
     		
     		
-    		console.log($scope.center);
+    	console.log($scope.center);
 
     		 });
     		
-    		// $scope.addMarkers = function() {
-      //           $scope.markers: {
-	     //                    center: {
-	     //                        lat: data.coords.latitude,
-	     //                        lng: data.coords.longitude,
-	     //                        message: "There you are!",
-	     //                    }   
-      //               }
-      //           });
-      //       };
+    	$scope.addMarkers = function() {
+            angular.extend($scope, {
+            	markers: {
+                	charleston: {
+                		lat: 32.907,
+                		lng: -79.802,
+                		message: "Chucktown"
+                	},
+                	iopbeach: {
+                    	lat: 32.79639,
+                    	lng: -79.765,
+              			message: "Isle of Palms Beach"
+                	},
+                	user: {
+                    	lat: $scope.center.lat,
+                    	lng: $scope.center.lng,
+              			message: "Here you are!"   
+                	}
+                }
+            });
+        };
 
-     
+ 		$scope.events = {
+                markers: {
+                    enable: leafletEvents.getAvailableMarkerEvents(),
+                }
+            };
 
+        $scope.eventDetected = "No events yet...";
+            var markerEvents = leafletEvents.getAvailableMarkerEvents();
+            for (var k in markerEvents){
+                var eventName = 'leafletDirectiveMarker.' + markerEvents[k];
+                $scope.$on(eventName, function(event, args){
+                    $scope.eventDetected = event.name;
+                });
+            }
+
+        $scope.removeMarkers = function() {
+                $scope.markers = {};
+            }
    
 
   }]);
