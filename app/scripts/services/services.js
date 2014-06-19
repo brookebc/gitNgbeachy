0a0d6b9bedbec36eb59f50c2bab98c68e5b1921f
brookebc <brooke.casey@gmail.com>
2014-06-19 17:03:24 -0400

    added a placeholder images for each of the beaches to display

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -10,7 +10,7 @@ angular.module('gitNgbeachyApp')
                         "lng": -79.765,
                         "message": "<a class='btn btn-success' href='#/beach/1'>Isle of Palms Beach</a>",
                         "id":1,
-                        "parking": "Residential parking"
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Sullivans Island",
@@ -18,7 +18,7 @@ angular.module('gitNgbeachyApp')
                         "lng": -79.83778,
                         "message": "<a class='btn btn-success' href='#/beach/2'>Sullivans Island Beach</a>",
                         "id":2,
-                        "parking": "Residential parking"
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Folly Beach",
@@ -26,49 +26,55 @@ angular.module('gitNgbeachyApp')
                         "lng": -79.939213,
                         "message": "<a class='btn btn-success' href='#/beach/3'>Folly Beach</a>",
                         "id":3,
-                        "parking": "Residential parking"
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "North Myrtle Beach",
                         "lat": 33.822216,
                         "lng": -78.680974,
                         "message": "<a class='btn btn-success' href='#/beach/4'>North Myrtle Beach</a>",
-                        "id":4
+                        "id":4,
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Edisto Beach",
                         "lat": 32.487613,
                         "lng": -80.324402,
                         "message": "<a class='btn btn-success' href='#/beach/5'>Edisto Beach</a>",
-                        "id":5
+                        "id":5,
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Surfside Beach",
                         "lat": 33.606111,
                         "lng": -78.973056,
                         "message": "<a class='btn btn-success' href='#/beach/6'>Surfside Beach</a>",
-                        "id":6
+                        "id":6,
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Litchfield Beach",
                         "lat": 33.4669,
                         "lng": -79.0997,
                         "message": "<a class='btn btn-success' href='#/beach/7'>Litchfield Beach</a>",
-                        "id":7
+                        "id":7,
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Pawley's Island Beach",
                         "lat": 33.425833,
                         "lng": -79.125,
                         "message": "<a class='btn btn success' href='#/beach/8'>Pawley's Island Beach</a>",
-                        "id":8
+                        "id":8,
+                        "image":"images/beach-44.jpg"
                         },
                         {
                         "name": "Huntington Beach State Park",
                         "lat": 33.5139,
                         "lng": -79.0611,
                         "message": "<a class='btn btn success' href='#/beach/9'>Huntington Beach State Park</a>",
-                        "id":9
+                        "id":9,
+                        "image":"images/beach-44.jpg"
                         }
                     
                          
